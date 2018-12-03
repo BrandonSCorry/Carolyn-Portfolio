c701cf752a8c3514deacde75c902f7440c15b2f6
Poltergeist <brandonssc@gmail.com>
2018-12-03 01:30:23 -0500

    update

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+$("#nav").load("components/nav.html");
+$("#footer").load("components/footer.html");
+$("#content").load("components/about.html");
+
+$( document ).ready(function() {
+  $(function () {
+
+    $(".navAbout").on("click", function () {
+      $("#sectionName").html('');
+
+      $("#content").load("components/about.html");
+    });
+    $(".navContact").on("click", function () {
+      $("#sectionName").html('');
+
+      $("#content").load("components/contact.html");
+    });
+    $(".workSections").on("click", "a", function () {
+
+      var module = this.id;
+      console.log(module);
+      var getthatmodule = ("./assets/data/" + module + ".json");
+      $("#content").empty;
+
+      $.getJSON(getthatmodule, function (json) {
+        moduleJson = json.artwork;
+        console.log(moduleJson);
+        $("#sectionName").show;
+        $("#sectionName").html(moduleJson[0].section + " - " + moduleJson[0].sectionname);
+        $("#sectionName").append("<div class='hr'></div>");
+
+        // $('div#content').html("<b>Section</b>: " + moduleJson[0].section);
+        $("#content").html(moduleJson.map(function (artwork) {
+          return ("<div class='grid-item'><a href=" + artwork.img + ' ' + "target='_blank'><div class='artGen' data-topic=''><img src='" + artwork.img + "'></div>" + "</a></div>");
+        }).join(" "));
+
+
+
+        //foreachLoop here
+
+
+      });
+
+      // $("#content").html("module" + module);
+      // $('#content').load("/sections/" + module + ".html");
+    });
+  });
+});
